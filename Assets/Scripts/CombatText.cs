cdca7574139c58b0cbf2fe1cb5905223e395fc0a
“WarTey” <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-10-28 05:01:48 +0100

    Affichage des dégâts

---
+++ Assets/Scripts/CombatText.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatText : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    [SerializeField] private Text text;
+
+    [SerializeField] private float lifeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    public IEnumerator FadeOut()
+    {
+        float startAlpha = text.color.a;
+
+        float rate = 1.0f / lifeTime;
+
+        float progress = 0.0f;
+
+        while (progress < 1.0)
+        {
+            Color temp = text.color;
+
+            temp.a = Mathf.Lerp(startAlpha, 0, progress);
+
+            text.color = temp;
+
+            progress += rate * Time.deltaTime;
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/CombatTextManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatTextManager : MonoBehaviour
+{
+    private static CombatTextManager instance;
+
+    public static CombatTextManager MyInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<CombatTextManager>();
+            }
+            return instance;
+        }
+    }
+
+    [SerializeField] private GameObject combatTextPrefab;
+
+    public void CreateText(Vector2 position, string text)
+    {
+        Text temp = Instantiate(combatTextPrefab, transform).GetComponent<Text>();
+        temp.transform.position = position;
+    }
+}
