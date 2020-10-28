03846a3436951ef17463c43024f1508e474599dd
“WarTey” <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-10-28 05:13:52 +0100

    Modification de l'affichage des dégâts

--- Assets/Scripts/CombatTextManager.cs
+++ Assets/Scripts/CombatTextManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum DamageType {DAMAGE, HEAL}
+
 public class CombatTextManager : MonoBehaviour
 {
     private static CombatTextManager instance;
@@ -21,9 +23,27 @@ public class CombatTextManager : MonoBehaviour
 
     [SerializeField] private GameObject combatTextPrefab;
 
-    public void CreateText(Vector2 position, string text)
+    public void CreateText(Vector2 position, string text, DamageType type)
     {
+        position.y += 1.0f;
+
         Text temp = Instantiate(combatTextPrefab, transform).GetComponent<Text>();
         temp.transform.position = position;
+
+        string operation = string.Empty;
+
+        switch (type)
+        {
+            case DamageType.DAMAGE:
+                operation += "-";
+                temp.color = Color.red;
+                break;
+            case DamageType.HEAL:
+                operation += "+";
+                temp.color = Color.green;
+                break;
+        }
+
+        temp.text = operation + text;
     }
 }
