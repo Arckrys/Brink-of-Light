450bafc23d7c4501de18a9d3bdac0398ea73e979
FlorianZeni <46035432+EolAyxee@users.noreply.github.com>
2020-11-02 19:45:09 +0100

    Debut Interface Razakus

---
+++ Assets/Scripts/RazakusMenuScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RazakusMenuScript : MonoBehaviour
+{
+
+    public void Start()
+    {
+
+    }
+
+    public void OnHealthPressed()
+    {
+        PlayerScript.MyInstance.LifeMaxValue += 10f;
+    }
+    public void OnAttackPressed()
+    {
+        Debug.Log("Attack pressed");
+        PlayerScript.MyInstance.AttackMaxValue += 10f;
+    }
+    public void OnRangePressed()
+    {
+        Debug.Log("Range pressed");
+        PlayerScript.MyInstance.RangeMaxValue += 10f;
+    }
+    public void OnCritChancePressed()
+    {
+        Debug.Log("Crit pressed");
+        PlayerScript.MyInstance.CritChanceMaxValue += 10f;
+    }
+    public void OnSpeedPressed()
+    {
+        Debug.Log("Speed pressed");
+        PlayerScript.MyInstance.MovementSpeedMaxValue += 10f;
+    }
+
+
+}
