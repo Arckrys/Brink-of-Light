d7bc9ad753d1daed686b7ec090e05be35e576c96
“WarTey” <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-11-04 18:50:42 +0100

    Modification des Stats

---
+++ Assets/Scripts/Stat/StatField.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatField : StatManager
+{
+    [SerializeField] private Text statValue;
+    
+    protected override void UpdateUIStat()
+    {
+        if (statValue)
+        {
+            statValue.text = MyCurrentValue.ToString(CultureInfo.InvariantCulture);
+        }
+        
+        base.UpdateUIStat();
+    }
+}
---
+++ Assets/Scripts/Stat/StatUI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatUI : StatManager
+{
+    private Image content;
+
+    [SerializeField] private bool scalable;
+
+    [SerializeField] private bool filled;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (filled)
+        {
+            content = GetComponent<Image>();
+        }
+    }
+
+    protected override void UpdateUIStat()
+    {
+        const float tolerance = 0.01f;
+        
+        if (scalable)
+        {
+            var scale = transform.localScale;
+
+            if (Math.Abs(scale.x - MyCurrentValue / MyMaxValue) > tolerance && Math.Abs(scale.y - MyCurrentValue / MyMaxValue) > tolerance)
+            {
+                scale.x = MyCurrentValue / MyMaxValue;
+                scale.y = MyCurrentValue / MyMaxValue;
+
+                transform.localScale = scale;
+            }
+        }
+        
+        if (filled && Math.Abs(content.fillAmount - MyCurrentValue / MyMaxValue) > tolerance)
+        {
+            content.fillAmount = MyCurrentValue / MyMaxValue;
+        }
+        
+        base.UpdateUIStat();
+    }
+}
