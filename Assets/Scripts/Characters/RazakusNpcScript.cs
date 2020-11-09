0bee0eeb1cf16271c7fbbaec947c0f59a69ee4af
nathanchevenard <nathan.chevenard@gmail.com>
2020-11-09 13:35:30 +0100

    Gestion du menu razakus dans l'objet Razakus, généralisation des Menus NPC

---
+++ Assets/Scripts/Characters/RazakusNpcScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RazakusNpcScript : VillageNpcScript
+{
+    private RazakusMenuScript razakusScript;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerInRange)
+        {
+            bool isMenuActivated = menuGameObject.activeSelf;
+
+            menuGameObject.SetActive(!menuGameObject.activeSelf);
+
+            if (!isMenuActivated)
+            {
+                razakusScript = RazakusMenuScript.MyInstance;
+                razakusScript.InitUI();
+            }
+
+            PlayerScript.MyInstance.SetIsInMenu(!PlayerScript.MyInstance.GetIsInMenu());
+        }
+    }
+}
