30cfd017af6fe8c468897a97aa84dd3a8393c7b5
FlorianZeni <46035432+EolAyxee@users.noreply.github.com>
2020-11-16 12:11:15 +0100

    Added Unfinished SaveSystem & updated all scenes

---
+++ Assets/Scripts/Core/GameData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+[System.Serializable]
+public class GameData
+{
+    
+    private double[] razakusPurchases;
+    public GameData()
+    {
+        razakusPurchases = RazakusMenuScript.MyInstance.GetRazakusPurchases();
+    }
+
+    public double[] getRazakusPurchases()
+    {
+        return razakusPurchases;
+    }
+}
---
+++ Assets/Scripts/Core/SaveSystem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    static string path = Application.persistentDataPath + "/BrinkOfLight.save";
+
+    public static void SaveGame()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        GameData data = new GameData();
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+        Debug.Log("Saved");
+    }
+
+    public static GameData LoadGame()
+    {
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            GameData data = (GameData) formatter.Deserialize(stream);
+            stream.Close();
+            Debug.Log("Loaded at " + path);
+            return data;
+        }
+        else
+        {
+            Debug.Log("Save File not Found in " + path);
+            return null;
+        }
+    }
+
+}
