a8c322815578e624364532efdd848d78fa869b1d
“WarTey” <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-11-21 03:23:57 +0100

    Ajout des transitions

--- Assets/Scripts/Environment/ExitManager.cs
+++ Assets/Scripts/Environment/ExitManager.cs
@@ -1,20 +1,51 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ExitManager : MonoBehaviour
 {
-    
+    [SerializeField] private float transitionSpeed;
+
+    [SerializeField] private GameObject transitionSpawn;
+
+    private void Start()
+    {
+        StartCoroutine(FadeOut());
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (!other.CompareTag("Player")) return;
+
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        var canvasGroup = GameObject.Find("CanvasTransition").GetComponent<CanvasGroup>();
+        while (canvasGroup.alpha < 1)
         {
-            Debug.Log("Exit");
+            yield return new WaitForSeconds(transitionSpeed);
+            canvasGroup.alpha += 0.1f;
         }
-        else
+
+        UpdatePlayerPosition();
+
+        StartCoroutine(FadeOut());
+    }
+    
+    private IEnumerator FadeOut()
+    {
+        var canvasGroup = GameObject.Find("CanvasTransition").GetComponent<CanvasGroup>();
+        while (canvasGroup.alpha > 0)
         {
-            Debug.Log(other.tag);
+            yield return new WaitForSeconds(transitionSpeed);
+            canvasGroup.alpha -= 0.1f;
         }
+    }
+
+    private void UpdatePlayerPosition()
+    {
+        PlayerScript.MyInstance.transform.position = transitionSpawn.transform.position;
     }
 }
