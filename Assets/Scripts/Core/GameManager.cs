36d12e65da08796930dcba087c41954de3226059
Nathan Chevenard <nathan.chevenard@gmail.com>
2020-11-24 16:34:47 +0100

    Chest spawn
    Un chest a une chance sur 15 de spawn après qu'une salle "normale" soit clear (la salle de départ comprise), le chest contient 3 loot bag et a une chance sur 4 de contenir un objet d'équipement en plus

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -14,9 +14,13 @@ public class GameManager : MonoBehaviour
     
     [SerializeField] private GameObject closeDoors;
     [SerializeField] private GameObject openDoors;
+    [SerializeField] private GameObject chestPrefab;
+
+    private bool enemiesAllKilled;
 
     private void Start()
     {
+        enemiesAllKilled = false;
         razakusScript = RazakusMenuScript.MyInstance;
         playerScript = PlayerScript.MyInstance;
         currencyScript = CurrenciesScript.MyInstance;
@@ -29,12 +33,24 @@ public class GameManager : MonoBehaviour
 
     private void UpdateDoorState()
     {
-        if (closeDoors != null && openDoors != null)
+        if (closeDoors != null && openDoors != null && !enemiesAllKilled)
         {
             if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
             {
                 closeDoors.SetActive(false);
                 openDoors.SetActive(true);
+                enemiesAllKilled = true;
+
+                if (DungeonFloorScript.MyInstance.GetCurrentNode().GetRoomType() == FloorNode.roomTypeEnum.regular)
+                {
+                    int randomInt = Random.Range(0, 15);
+                    if (randomInt == 0)
+                    {
+                        print("spawning chest");
+                        GameObject chest = Instantiate(chestPrefab);
+                        chest.transform.parent = GameObject.FindGameObjectWithTag("Room").transform;
+                    }
+                }
             }
             else
             {
@@ -60,6 +76,7 @@ public class GameManager : MonoBehaviour
     {
         closeDoors = null;
         openDoors = null;
+        enemiesAllKilled = false;
     }
 
     public static GameManager MyInstance
---
+++ Assets/Scripts/Environment/ChestScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestScript : MonoBehaviour
+{
+    private bool isOpen;
+    private Animator chestAnimator;
+
+    [SerializeField] private GameObject lootBag;
+
+    private void Start()
+    {
+        chestAnimator = GetComponent<Animator>();
+        isOpen = false;
+    }
+
+    private void Update()
+    {
+        chestAnimator.SetBool("isOpened", isOpen);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !isOpen)
+        {
+            isOpen = true;
+
+            //create 3 bags
+            string randomConsumableItem = null;
+            int consumableItemProbability = Random.Range(0, 25);
+
+            var itemManager = ItemsManagerScript.MyInstance;
+
+            if (consumableItemProbability == 0)
+                randomConsumableItem = itemManager.SelectRandomItem(itemManager.ConsumableItems);
+
+            for (int i = 0; i < 3; i++)
+            {
+                var loot = Instantiate(lootBag, new Vector2(transform.position.x - 1 + i, transform.position.y - 1), Quaternion.identity);
+                loot.transform.parent = this.transform.parent;
+                loot.GetComponent<LootManager>().CreateBag(randomConsumableItem, 0, Random.Range(0, 3), Random.Range(0, 6));
+            }
+
+            //has a small probability to spawn an equipment item
+            int equipmentItemProbability = Random.Range(0, 4);
+
+            if (equipmentItemProbability == 0)
+            {
+                string randomEquipmentItem = itemManager.SelectRandomItem(itemManager.EquipmentItems);
+                var item = itemManager.CreateEquipmentItem(new Vector2(transform.position.x, transform.position.y + 1), randomEquipmentItem);
+                item.transform.parent = this.transform.parent;
+            }
+        }
+    }
+}
