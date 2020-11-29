fd1ac21e4232af405a6026e3feb555134b253079
“WarTey” <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-11-29 07:03:31 +0100

    Ajout du menu principal

---
+++ Assets/Scripts/Core/MainMenuManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class MainMenuManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private GameObject fire;
+
+    private Coroutine fireCoroutine;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        var tempColor = fire.GetComponent<SpriteRenderer>().color;
+        tempColor.a = 0;
+        fire.GetComponent<SpriteRenderer>().color = tempColor;
+        fire.GetComponent<Light2D>().intensity = 0;
+    }
+    
+    private IEnumerator OffFire()
+    {
+        var tempColor = fire.GetComponent<SpriteRenderer>().color;
+        while (tempColor.a > 0f)
+        {
+            tempColor.a -= 0.1f;
+            fire.GetComponent<Light2D>().intensity -= 0.1f;
+            fire.GetComponent<SpriteRenderer>().color = tempColor;
+            yield return new WaitForSeconds(0.1f);
+        }
+        StopCoroutine(fireCoroutine);
+    }
+
+    private IEnumerator OnFire()
+    {
+        var tempColor = fire.GetComponent<SpriteRenderer>().color;
+        while (tempColor.a < 1f)
+        {
+            tempColor.a += 0.1f;
+            fire.GetComponent<Light2D>().intensity += 0.1f;
+            fire.GetComponent<SpriteRenderer>().color = tempColor;
+            yield return new WaitForSeconds(0.1f);
+        }
+        StopCoroutine(fireCoroutine);
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (fireCoroutine != null)
+        {
+            StopCoroutine(fireCoroutine);
+        }
+        
+        fireCoroutine = StartCoroutine(OnFire());
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (fireCoroutine != null)
+        {
+            StopCoroutine(fireCoroutine);
+        }
+        
+        fireCoroutine = StartCoroutine(OffFire());
+    }
+}
