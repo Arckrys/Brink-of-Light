553c9cba8580d14e23130b8ccf72e8171469f6cb
Arckrys <aymeric.damour06@gmail.com>
2020-12-03 12:45:42 +0100

    amélioration chauve-souris

---
+++ Assets/Scripts/Enemies/BatScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatScript : DistanceScript
+{
+    protected override void shoot(Vector2 direction)
+    {
+        FireProjectileAtDirection(Rotate(direction,5f));
+        FireProjectileAtDirection(Rotate(direction, -5f));
+    }
+}
