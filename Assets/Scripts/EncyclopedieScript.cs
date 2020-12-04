f691fc420f42c083cd673d5e09fcf2d4c1f149e9
FlorianZeni <46035432+EolAyxee@users.noreply.github.com>
2020-12-04 15:35:09 +0100

    Added Encyclopedia

---
+++ Assets/Scripts/EncyclopedieScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncyclopedieScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject menuEncyclopedie;
+    [SerializeField] private GameObject itemsScroll;
+    [SerializeField] private GameObject mobsScroll;
+
+    private bool isItemShown = true;
+    private bool isInCollision = false;
+
+    public void Start()
+    {
+        menuEncyclopedie.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.E) || !isInCollision) return;
+
+        menuEncyclopedie.SetActive(!menuEncyclopedie.activeSelf);
+        PlayerScript.MyInstance.SetIsInMenu(!PlayerScript.MyInstance.GetIsInMenu());
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+
+        isInCollision = true;
+
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+
+        isInCollision = false;
+
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+
+    /* Affiche le canvas des items (bouton) */
+    public void showItems()
+    {
+        if (!isItemShown)
+        {
+            isItemShown = true;
+            itemsScroll.SetActive(true);
+            mobsScroll.SetActive(false);
+        }
+    }
+
+    /* Affiche le canvas des ennemis (bouton) */
+    public void showMobs()
+    {
+        if (isItemShown)
+        {
+            isItemShown = false;
+            itemsScroll.SetActive(false);
+            mobsScroll.SetActive(true);
+        }
+    }
+}
