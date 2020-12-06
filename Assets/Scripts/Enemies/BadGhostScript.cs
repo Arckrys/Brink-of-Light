b8a2560235f123effa953ceda2cb3fb485531a23
Arckrys <aymeric.damour06@gmail.com>
2020-12-06 21:44:30 +0100

    Ajout du mauvais esprit

---
+++ Assets/Scripts/Enemies/BadGhostScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadGhostScript : DistanceScript
+{
+    protected override void shoot(Vector2 direction)
+    {
+        FireProjectileAtDirection(Rotate(direction,90f));
+        FireProjectileAtDirection(Rotate(direction, -90f));
+        FireProjectileAtDirection(direction);
+    }
+}
