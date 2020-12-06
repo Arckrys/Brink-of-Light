1f75abe096c1e52f4e4581e5ef20001b31495deb
“WarTey” <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-12-06 09:51:32 +0100

    Avancement du scénario

--- Assets/Scripts/Core/OpeningManager.cs
+++ Assets/Scripts/Core/OpeningManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpeningManager : MonoBehaviour
 {
@@ -9,8 +10,12 @@ public class OpeningManager : MonoBehaviour
     [SerializeField] private List<Dialogue> dialogue;
 
     [SerializeField] private Animator loadAnimator;
+    
+    [SerializeField] private Animator flashAnimator;
 
     [SerializeField] private List<GameObject> scene;
+
+    [SerializeField] private Image ImageLoader;
 
     private int indexDialogue;
     
@@ -25,14 +30,29 @@ public class OpeningManager : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        // TODO : Check in transition
         if (DialogueManagerScript.MyInstance.SentenceIsOver && Input.GetKeyDown(KeyCode.E))
         {
             indexDialogue += 1;
 
+            print(indexDialogue);
+
             if (indexDialogue < dialogue.Count)
             {
-                StartCoroutine(FadeOutDialogue());
-                StartCoroutine(ChangeScene());
+                if (ImageLoader.color.Equals(Color.white))
+                {
+                    ImageLoader.color = Color.black;
+                }
+
+                if (indexDialogue == 4)
+                {
+                    StartCoroutine(FlashScene());
+                }
+                else
+                {
+                    StartCoroutine(FadeOutDialogue());
+                    StartCoroutine(ChangeScene());
+                }
             }
         }
     }
@@ -52,6 +72,21 @@ public class OpeningManager : MonoBehaviour
         loadAnimator.SetTrigger("End");
         
         StartCoroutine(FadeInDialogue());
+    }
+
+    private IEnumerator FlashScene()
+    {
+        flashAnimator.SetTrigger("Start");
+        
+        yield return new WaitForSeconds(0.4f);
+        
+        if (indexDialogue < scene.Count)
+        {
+            scene[indexDialogue - 1].SetActive(false);
+            scene[indexDialogue].SetActive(true);
+        }
+        
+        DialogueManagerScript.MyInstance.StartDialogue(dialogue[indexDialogue]);
     }
 
     private IEnumerator FadeInDialogue()
