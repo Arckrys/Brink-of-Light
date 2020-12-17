41aaeaf2be9f97362d31811d183962c6c90a1b05
“WarTey” <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-12-18 00:31:34 +0100

    Fermeture du menu Urbius et Igeirus avec la touche echap

--- Assets/Scripts/Characters/NpcScript.cs
+++ Assets/Scripts/Characters/NpcScript.cs
@@ -17,7 +17,7 @@ public class NpcScript : VillageNpcScript
 
     private void Update()
     {
-        if (!Input.GetKeyDown(KeyCode.E) || !isInCollision) return;
+        if (!Input.GetKeyDown(KeyCode.E) && (!Input.GetKeyDown(KeyCode.Escape) || !PlayerScript.MyInstance.GetIsInMenu()) || !isInCollision) return;
 
         // Activates the seller's menu
         if (isSeller && sellerDialogueDone && DialogueManagerScript.MyInstance.SentenceIsOver)
@@ -34,32 +34,31 @@ public class NpcScript : VillageNpcScript
             {
                 case NPCName.Razakus:
                     RazakusMenuScript.MyInstance.InitUI();
-                    if(!sellerDialogueDone)
+                    if (!sellerDialogueDone)
                         TriggerDialogue();
                     break;
                 case NPCName.Igeirus:
                 case NPCName.Urbius:
-                    {
-                        seller.MySellerName = SellerName;
-                        seller.UpdateUI();
-                        if (!sellerDialogueDone)
-                            TriggerDialogue();
-                        break;
-                    }
+                {
+                    seller.MySellerName = SellerName;
+                    seller.UpdateUI();
+                    if (!sellerDialogueDone)
+                        TriggerDialogue();
+                    break;
+                }
                 case NPCName.Talker:
-                    {
-                        if (!DialogueManagerScript.MyInstance.DialogueIsOpen)
-                            TriggerDialogue();
-                        break;
-                    }
+                {
+                    if (!DialogueManagerScript.MyInstance.DialogueIsOpen)
+                        TriggerDialogue();
+                    break;
+                }
 
                 default:
                     throw new ArgumentOutOfRangeException();
             }
-            
+
             sellerDialogueDone = true;
         }
-
     }
     public void TriggerDialogue()
     {
